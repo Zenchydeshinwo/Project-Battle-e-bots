81920f6be691365042dd45a3581d458148d19069
Zenchy <alvaro.corral.soilan@gmail.com>
2019-01-21 20:12:17 +0100

    Se mueve pero seguido--->conseguir que sea con el cursor

--- js/bots.js
+++ js/bots.js
@@ -1,42 +1,42 @@
 function Bot(bot, botX, vx, vy){
 
-this.game=bot,
-this.x=botX,
-this.vx=vx,
-this.vy=vy
-
-
-}
-
-Bot.prototype.drawBot=function()
-{
+    this.game=bot,
+    this.x=botX,
+    this.vx=vx,
+    this.vy=vy
     
     
-    this.game.ctx.fillStyle="red"
-    this.game.ctx.fillRect(this.x+this.vx, 150+this.vy, 60, 90)
-    console.log("Hi")
-}
-    
-Bot.prototype.moveBot=function(expression)
-{
-    switch(expression) 
-    {
-        case 37:
-          this.vx-=1
-          break;
-        case 38:
-          this.vy-=1
-          break;
-        case 39:
-          this.vx+=1
-          break;
-        case 40:
-          this.vy+=1
-          break;
-        default:
-          
     }
- console.log(">>>>>>>>>"+this.vx+">>>>>>>>>>>"+this.vy)
-
-}
-
+    
+    Bot.prototype.drawBot=function()
+    {
+    
+    
+        this.game.ctx.fillStyle="red"
+        this.game.ctx.fillRect(this.x+this.vx, 150+this.vy, 60, 90)
+        console.log("Hi")
+    }
+    
+    Bot.prototype.moveBot=function(expression)
+    {
+        switch(expression) 
+        {
+            case 1:
+              this.vx-=10
+              break;
+            case 2:
+              this.vy-=10
+              break;
+            case 3:
+              this.vx+=10
+              break;
+            case 4:
+              this.vy+=10
+              break;
+            default:
+    
+        }
+     console.log(">>>>>>>>>"+this.vx+">>>>>>>>>>>"+this.vy)
+    
+    }
+    
--- js/game.js
+++ js/game.js
@@ -1,56 +1,72 @@
 //Cosntructor juego
 window.onload=function()
 {
-    
+
 
     var Game = 
     {
-
+        sign:0,
         canvas:undefined,
         ctx:undefined,
-        
+
         start:function()
         {
 
-            //this.moveBot()
+            this.dirBot()
+            this.bot.moveBot(this.sign)
             this.canvas=document.getElementById("ring")
             this.ctx=this.canvas.getContext("2d")
 
-            this.interval =setInterval(function(){
+            this.interval=setInterval(function(){
 
                 //console.log(this)
                 this.ctx.clearRect(0,0,1200,1000)
                 this.reDrawBot()
 
-            }.bind(this),1000/60)
+            }.bind(this),500)
 
 
-            
+
 
         },
         startBot:function()
         {
-            
+
             //var x=function() {return Math.floor(Math.random()*800)+100}
-            
+
             this.bot=new Bot(this,600,10,25)
             //this.bot2 = new Bot(this, 200,25,60)
-            
-            
+
+
+        },
+
+        dirBot:function()
+        {
+            document.onkeydown = function(e) {
+                
+                switch (e.keyCode) {
+                    
+                  case 38: this.sign=2;  break;
+                  case 40: this.sign=4;  break;
+                  case 37: this.sign=1;  break;
+                  case 39: this.sign=3;  break;
+                  console.log(">>>>>>>>>>>>>>>"+this.sign)
+                }
+            }.bind(Game)
         },
 
         reDrawBot:function()
         {   
-            this.bot.moveBot(37)
+            
             this.bot.drawBot()
             //this.bot2.drawBot()
         }
 
-        
-        
+
+
     }
 
-    
+
 
 Game.start();
 Game.startBot();
