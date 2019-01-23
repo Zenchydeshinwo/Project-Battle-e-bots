f6d8110d19cc8d108540e5c8dc5aad3ae6ff42c8
Zenchy <alvaro.corral.soilan@gmail.com>
2019-01-23 01:43:38 +0100

    prueba rara, borrar est, en bot, y cambiar el window que se pasa a bot, por this

--- js/bots.js
+++ js/bots.js
@@ -1,25 +1,28 @@
-function Bot(game)
+function Bot(esto)
     {
     
-    this.game=game,
+    this.game=esto.game,
     this.contador=0,
     this.counter=0,
     this.vx=window.innerWidth*0.08,
     this.vy=window.innerHeight*.8,
     this.img = new Image();
     this.img.src = './img/bot1.png';
+    console.log("++++++++++++"+this)
+
     }
     
     Bot.prototype.drawBot=function()
     {
-        this.counter+=1
-
-        if(this.counter>25)
-        {
-            this.contador=this.contador+1
-            this.counter=0
-        }
-        if (this.contador%3==0) {this.contador=0}
+        // this.counter+=1
+        // console.log(">>>>>>>>>>>>>"+this)
+        // //debugger
+        // if(this.counter>25)
+        // {
+        //     this.contador=this.contador+1
+        //     this.counter=0
+        // }
+        // if (this.contador%3==0) {this.contador=0}
 
         this.game.ctx.drawImage(this.img, 75*this.contador, 0, 70, 70, this.vx, this.vy, window.innerWidth*0.05 ,window.innerHeight*0.1)
         
@@ -35,6 +38,8 @@ function Bot(game)
 
 
     }
+
+    //Bot.prototype.animeBot=function(this)
     
 
 
--- js/game.js
+++ js/game.js
@@ -6,25 +6,23 @@ window.onload=function()
         canvas:undefined,
         ctx:undefined,
         factorFrame:60,
-        //counterFrame:0,
+        fractionFrame:100,
+        // counterFrame:0,
         start:function()
         {
-            this.background()
-            this.dirBot()
+            this.reset()
             this.canvas=document.getElementById("ring")
             this.ctx=this.canvas.getContext("2d")
             this.canvas.width=window.innerWidth
             this.canvas.height=window.innerHeight
-            this.interval=setInterval(function()
-                {
-                this.background.draw()
-                this.reDrawBot()
-                }.bind(this),100/this.factorFrame)
+            this.chrono()
+            this.background()
+            this.dirBot()
         },
         startBot:function()
         {
             
-            this.bot=new Bot(this)
+            this.bot=new Bot(window)
 
         },
         dirBot:function()
@@ -33,6 +31,7 @@ window.onload=function()
             {                
                 
                  this.bot.moveBot(e.keyCode)
+                 if (e.keyCode==80){this.stop()}
                 
             }.bind(this)
         },
@@ -47,9 +46,25 @@ window.onload=function()
         },
         stop: function () {
             clearInterval(this.interval);
-          },
+        },
+
+        chrono:function()
+        {
+            this.interval=setInterval(function()
+                {
+                this.counterFrame++
+                if (this.fractionFrame==this.counterFrame){this.counterFrame=0}
+                this.background.draw()
+                this.reDrawBot()
+                
+                }.bind(this),this.fractionFrame/this.factorFrame)
+        },
+        reset:function(){
+            this.counterFrame=0
+        }
     }
     Game.startBot();
     Game.start();
+    console.log("========D"+this)
 } 
 
